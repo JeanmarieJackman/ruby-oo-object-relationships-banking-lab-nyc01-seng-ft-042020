1dfcf06237119273bdca02f7199c80ece0944ba7
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-04-23 16:10:35 -0400

    update bank account
    after pair coding with David Spector

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class BankAccount
     attr_reader :name
     attr_accessor :balance, :status
@@ -8,29 +6,18 @@ class BankAccount
     @balance = 1000
     @status = "open"
     end
-
     def deposit(amount)
         self.balance += amount
     end
-
     def display_balance
         "Your balance is $#{@balance}."
     end
     # ask is self.balance == @balance? if not, which is better and why?
     def valid?
-        if
-            @status == "open" &&
-            @balance > 0
-            true
-        else
-            false
-        end
+        @status == "open" && @balance > 0
     end
-
     def close_account
         @status = "closed"
     end
     # ask is self.status == @status? if not, which is better and why?
-    
-
 end
